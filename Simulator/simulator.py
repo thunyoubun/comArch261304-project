bf3c7f9fa6b3afaa0ba2c9676e8924e2376ccf9d
jirayu <jirayu004za@gmail.com>
2023-10-05 00:08:28 +0700

    add note process

--- Simulator/simulator.py
+++ Simulator/simulator.py
@@ -93,44 +93,44 @@ class Simulator:
         
         return opcode, arg0, arg1, arg2
 
-    def executeInstructionType_R(self, opcode, arg0, arg1, dest):
-        if not self.isValidRegister(arg0) or not self.isValidRegister(arg1) or not self.isValidRegister(dest):
-            self.exceptionError(f"Register must be a valid register {arg0}, {arg1}, {dest}")
+    def executeInstructionType_R(self, opcode, arg0, arg1, dest):                                                       # execute instruction R type
+        if not self.isValidRegister(arg0) or not self.isValidRegister(arg1) or not self.isValidRegister(dest):          # check if register(rs), (rt), (dest) is invalid 
+            self.exceptionError(f"Register must be a valid register {arg0}, {arg1}, {dest}")                            # throw exception show and value rs, rt, dest
 
-        if opcode == 0:
-            self.state['reg'][dest] = self.state['reg'][arg0] + self.state['reg'][arg1]
-        elif opcode == 1:
+        if opcode == 0:                                                                                        # check if opcode is ADD elif NOR                   
+            self.state['reg'][dest] = self.state['reg'][arg0] + self.state['reg'][arg1]                        # assign ....
+        elif opcode == 1:                                                   
             self.state['reg'][dest] = ~(self.state['reg'][arg0] | self.state['reg'][arg1])
-        else:
+        else:                                                                                                  # else throw exception
             self.exceptionError("Invalid opcode" + str(opcode))
     
-    def executeInstructionType_I(self, opcode, arg0, arg1, offset):
-        offset = self.convertNumber(offset)
+    def executeInstructionType_I(self, opcode, arg0, arg1, offset):                                                     # execute instruction I type
+        offset = self.convertNumber(offset)                                                                             # assign offset to 2's complement
 
-        if not self.isValidRegister(arg0) or not self.isValidRegister(arg1):
-            self.exceptionError(f"Register must be a valid register {arg0}, {arg1}")
+        if not self.isValidRegister(arg0) or not self.isValidRegister(arg1):                                            # check if register(rs), (rt) is invalid 
+            self.exceptionError(f"Register must be a valid register {arg0}, {arg1}")                                    # throw exception and show value rs, rt
         
-        if offset > 32767 or offset < -32768:
-            self.exceptionError("Out of range offset")
+        if offset > 32767 or offset < -32768:                                                                           # check if offset is more than maximum positive value or less than minimum negative value 
+            self.exceptionError("Out of range offset")                                                                  # throw exception                                        
 
-        if opcode == 2:
-            self.state['reg'][arg1] = self.state['mem'][self.state['reg'][arg0] + offset]
+        if opcode == 2:                                                                                        # check if opcode is LW elif SW elif BEQ     
+            self.state['reg'][arg1] = self.state['mem'][self.state['reg'][arg0] + offset]                      # assign ....
         elif opcode == 3:
             self.state['mem'][self.state['reg'][arg0] + offset] = self.state['reg'][arg1]
         elif opcode == 4:
             if self.state['reg'][arg0] == self.state['reg'][arg1]:
                 self.state['pc'] += offset
-        else:
+        else:                                                                                                  # else throw exception
             self.exceptionError("Invalid opcode" + str(opcode))
 
-    def executeInstructionType_J(self, opcode, arg0, arg1):
-        if not self.isValidRegister(arg0) or not self.isValidRegister(arg1):
-            self.exceptionError(f"Register must be a valid register {arg0}, {arg1}")
+    def executeInstructionType_J(self, opcode, arg0, arg1):                                                             # execute instruction J type
+        if not self.isValidRegister(arg0) or not self.isValidRegister(arg1):                                            # check if register(rs), (rt) is invalid
+            self.exceptionError(f"Register must be a valid register {arg0}, {arg1}")                                    # throw exception and show value rs, rt
 
-        if opcode == 5:
-            self.state['reg'][arg1] = self.state['pc']
+        if opcode == 5:                                                                                        # check if opcode is JARL
+            self.state['reg'][arg1] = self.state['pc']                                                         # assign ....
             self.state['pc'] = self.state['reg'][arg0]
-        else:
+        else:                                                                                                  # else throw exception
             self.exceptionError("Invalid opcode" + str(opcode))
 
     def executeTask(self):
@@ -141,17 +141,17 @@ class Simulator:
             print((f"memory[{i}] = {self.state['mem'][i]}"))
             self.tempString.append(f"memory[{i}] = {self.state['mem'][i]}")
 
-        self.printState()                                                                # print first state, registers indexes is zero                                  
+        self.printState()                                                                    # print first state, registers indexes is zero                                  
         self.tempString.append(self.formatOutputString())                                    # append first state follow format output print state
 
-        while not self.halt:
-            opcode, arg0, arg1, arg2 = self.parseInstructionFromMemory()
+        while not self.halt:                                                            # loop until halt equals true
+            opcode, arg0, arg1, arg2 = self.parseInstructionFromMemory()                # parse instruction from memory
 
-            self.state['pc'] += 1
-            self.executionCount += 1
+            self.state['pc'] += 1                                                       # increment pc + 1                                 
+            self.executionCount += 1                                                    # increment execution count + 1
 
-            if self.state['pc'] < 0 or self.state['pc'] >= self.maximumMemory:
-                self.exceptionError("PC out of memory!!!")
+            if self.state['pc'] < 0 or self.state['pc'] >= self.maximumMemory:          # check if pc less than 0 or greater than maximum memory
+                self.exceptionError("PC out of memory!!!")                              # throw exception                           
 
             if opcode == 0 or opcode == 1:                                              # case 0: ADD or case 1: NOR
                 self.executeInstructionType_R(opcode, arg0, arg1, arg2)                 # execute instruction type R
